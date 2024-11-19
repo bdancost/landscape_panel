67f04a20b23557ee4452358578683e96f36b778f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-19 17:03:08 -0300

    Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    description: "Uma foto teste",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    description: "Gato brincando com um novelo de lã",
+    image: "https://placekitten.com/400/300",
+  },
+  {
+    id: 3,
+    description: "Gatinho dormindo em uma caixa",
+    image: "https://placekitten.com/200/200",
+  },
+  {
+    id: 4,
+    description: "Gato olhando pela janela",
+    image: "https://placekitten.com/300/250",
+  },
+  {
+    id: 5,
+    description: "Dois gatos brincando",
+    image: "https://placekitten.com/500/400",
+  },
+  {
+    id: 6,
+    description: "Gatinho comendo ração",
+    image: "https://placekitten.com/350/300",
+  },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Hello from the backend");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
