4b16298d35b180666dc024780ff7c17349dbcc9f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-20 13:59:33 -0300

    Definindo a estrutura do projeto com as pastas de arquivos organizadas e linkadas corretamente

--- server.js
+++ server.js
@@ -1,56 +1,11 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-const posts = [
-  {
-    id: 1,
-    description: "Uma foto teste",
-    image: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    description: "Gato brincando com um novelo de lã",
-    image: "https://placekitten.com/400/300",
-  },
-  {
-    id: 3,
-    description: "Gatinho dormindo em uma caixa",
-    image: "https://placekitten.com/200/200",
-  },
-  {
-    id: 4,
-    description: "Gato olhando pela janela",
-    image: "https://placekitten.com/300/250",
-  },
-  {
-    id: 5,
-    description: "Dois gatos brincando",
-    image: "https://placekitten.com/500/400",
-  },
-  {
-    id: 6,
-    description: "Gatinho comendo ração",
-    image: "https://placekitten.com/350/300",
-  },
-];
-
+// Inicializa o servidor Express
 const app = express();
-app.use(express.json());
+routes(app);
 
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
