1261a96e5635f559a13a8f6b74195a65482aa283
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-21 18:52:44 -0300

    Implementando Armazenamento e Upload de Imagens

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,7 +4,7 @@ import conectarAoBanco from "../config/dbConfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para obter todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
   // Seleciona o banco de dados 'imersao-instabytes'
   const db = conexao.db("imersao-instabytes");
 
@@ -13,4 +13,10 @@ export default async function getTodosPosts() {
 
   // Busca todos os documentos da coleção e retorna como um array
   return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instabytes");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  listarPosts,
+  postarNovoPost,
+  uploadImagem,
+} from "../controllers/postsController.js";
+
+const upload = multer({ dest: "./uploads" });
 
 const routes = (app) => {
   // Permite que o servidor interprete requisições com corpo JSON
   app.use(express.json());
 
-  // Rota para obter todos os posts
+  // Rota para buscar todos os posts
   app.get("/posts", listarPosts);
+  // Rota para criar um novo post
+  app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
